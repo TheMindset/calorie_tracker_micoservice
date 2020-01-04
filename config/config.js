97ed29bd24614bb2fec20446577c503c148fe08c
TheMindset <mulezig2014@outlook.fr>
2020-01-04 06:54:19 +0100

    Use postgres

--- config/config.js
+++ config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": null,
     "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
-    "dialect": "mysql",
+    "dialect": "postgres",
     "operatorsAliases": 0
   },
   "test": {
@@ -12,7 +14,7 @@ module.exports = {
     "password": null,
     "database": "calorie_tracker_microservice_test",
     "host": "127.0.0.1",
-    "dialect": "mysql",
+    "dialect": "postgres",
     "operatorsAliases": 0
   },
   "production": {
@@ -20,7 +22,7 @@ module.exports = {
     "password": null,
     "database": "calorie_tracker_microservice_production",
     "host": "127.0.0.1",
-    "dialect": "mysql",
+    "dialect": "postgres",
     "use_env_variable": "DATABASE_URL",
     "operatorsAliases": 0
   }
