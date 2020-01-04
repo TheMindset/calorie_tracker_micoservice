d7fdd72ac7cc9c7593470a9297ec168fa2897c36
TheMindset <mulezig2014@outlook.fr>
2020-01-04 08:52:43 +0100

    Add APP_ID & APP_KEY,  require model Recipe, require recipeSerializer, refactor fetch url

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,16 @@
+require('dotenv').config()
+
 const { task, desc } = require('jake')
 const fetch = require('node-fetch')
+
+const Recipe = require('./models').Recipe
+
+const recipeSerializer = require('./serializers/recipe_serializer')
 
 desc('Seed database from Edaman API.')
 
 task('fetchData', {async: true }, (food) => {
-  fetch(`https://api.edamam.com/search?q=${food}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&from=0&to=5`)
+  fetch(`https://api.edamam.com/search?q=${food}&app_id=${process.env.EDAMAN_APP_ID}&app_key=${process.env.EDAMAN_APP_KEY}&from=0&to=5&time=1%2B`)
   .then(response => {
     return response.json()
   })
